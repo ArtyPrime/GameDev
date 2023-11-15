2dc390c04a2dfa72aee31ea4cf0886d8a1497dc6
ArtyPrime <frost2077eg@gmail.com>
2023-11-14 23:30:59 -0500

    Made new animation
    This tutorial is more in-depth than the other one

--- My Godot Projects/2D Puzzle/Player/Player_model.gd
+++ My Godot Projects/2D Puzzle/Player/Player_model.gd
@@ -2,38 +2,39 @@ extends CharacterBody2D
 
 
 const SPEED = 400.0
-const JUMP_VELOCITY = -900.0
-@onready var sprite_2d = $Sprite2D
+const JUMP_VELOCITY = -800.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var animation = get_node("AnimationPlayer")
 
 
 func _physics_process(delta):
-	#Animation
-	if(velocity.x > 1 || velocity.x < -1):
-		sprite_2d.animation = "running"
-	else:
-		sprite_2d.animation = "idle"
-	
+
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-		sprite_2d.animation = "jumping"
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+		animation.play("Jump")
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("left", "right")
+	print(direction)
+	if direction == -1:
+		get_node("Sprite2D").flip_h = true
+	elif direction == 1:
+		get_node("Sprite2D").flip_h = false
 	if direction:
 		velocity.x = direction * SPEED
+		if velocity.y == 0:
+			animation.play("Run")
 	else:
-		velocity.x = move_toward(velocity.x, 0, 25)
-
+		velocity.x = move_toward(velocity.x, 0, 100)
+		if velocity.y == 0:
+			animation.play("Idle")
+	#if velocity.y == 0:
+	#	animation.play("Fall")
 	move_and_slide()
 	
-	#Depeding on the last input, change the orientation of the sprite
-	var isLeft = velocity.x < 0
-	sprite_2d.flip_h = isLeft
